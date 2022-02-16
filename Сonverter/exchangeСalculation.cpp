2e1f6704db106cdd9630820b87677200379cb1a6
Alexey Burak <byraklesha@gmail.com>
2022-02-16 20:59:50 +0300

    add func

--- Сonverter/exchangeСalculation.cpp
+++ Сonverter/exchangeСalculation.cpp
@@ -6,7 +6,7 @@ double eur_usd(double &exchangeAmount, double &resultOfExchange)
 {
     cout << "What amount in EUR do you want to exchange?"<<endl;
     cin >> exchangeAmount;
-    resultOfExchange = exchangeAmount * 1.9;
+    resultOfExchange = exchangeAmount * eur_usd_course;
     return resultOfExchange;
 
 }
@@ -14,133 +14,133 @@ double eur_byn(double &exchangeAmount, double &resultOfExchange)
 {
     cout << "What amount in EUR do you want to exchange?"<<endl;
     cin >> exchangeAmount;
-    resultOfExchange = exchangeAmount * 1.9;
+    resultOfExchange = exchangeAmount * eur_byn_course;
     return resultOfExchange;
 }
 double eur_rub(double &exchangeAmount, double &resultOfExchange)
 {
     cout << "What amount in EUR do you want to exchange?"<<endl;
     cin >> exchangeAmount;
-    resultOfExchange = exchangeAmount * 1.9;
+    resultOfExchange = exchangeAmount * eur_rub_course;
     return resultOfExchange;
 }
 double eur_uah(double &exchangeAmount, double &resultOfExchange)
 {
     cout << "What amount in EUR do you want to exchange?"<<endl;
     cin >> exchangeAmount;
-    resultOfExchange = exchangeAmount * 1.9;
+    resultOfExchange = exchangeAmount * eur_uah_course;
     return resultOfExchange;
 }
 double usd_eur(double &exchangeAmount, double &resultOfExchange)
 {
     cout << "What amount in USD do you want to exchange?"<<endl;
     cin >> exchangeAmount;
-    resultOfExchange = exchangeAmount * 1.9;
+    resultOfExchange = exchangeAmount * usd_eur_course;
     return resultOfExchange;
 }
 double usd_byn(double &exchangeAmount, double &resultOfExchange)
 {
     cout << "What amount in USD do you want to exchange?"<<endl;
     cin >> exchangeAmount;
-    resultOfExchange = exchangeAmount * 1.9;
+    resultOfExchange = exchangeAmount * usd_byn_course;
     return resultOfExchange;
 }
 double usd_rub(double &exchangeAmount, double &resultOfExchange)
 {
     cout << "What amount in USD do you want to exchange?"<<endl;
     cin >> exchangeAmount;
-    resultOfExchange = exchangeAmount * 1.9;
+    resultOfExchange = exchangeAmount * usd_rub_course;
     return resultOfExchange;
 }
 double usd_uah(double &exchangeAmount, double &resultOfExchange)
 {
     cout << "What amount in USD do you want to exchange?"<<endl;
     cin >> exchangeAmount;
-    resultOfExchange = exchangeAmount * 1.9;
+    resultOfExchange = exchangeAmount * usd_uah_course;
     return resultOfExchange;
 }
 double byn_eur(double &exchangeAmount, double &resultOfExchange)
 {
     cout << "What amount in BYN do you want to exchange?"<<endl;
     cin >> exchangeAmount;
-    resultOfExchange = exchangeAmount * 1.9;
+    resultOfExchange = exchangeAmount * byn_eur_course;
     return resultOfExchange;
 }
 double byn_usd(double &exchangeAmount, double &resultOfExchange)
 {
     cout << "What amount in BYN do you want to exchange?"<<endl;
     cin >> exchangeAmount;
-    resultOfExchange = exchangeAmount * 1.9;
+    resultOfExchange = exchangeAmount * byn_usd_course;
     return resultOfExchange;
 }
 double byn_rub(double &exchangeAmount, double &resultOfExchange)
 {
     cout << "What amount in BYN do you want to exchange?"<<endl;
     cin >> exchangeAmount;
-    resultOfExchange = exchangeAmount * 1.9;
+    resultOfExchange = exchangeAmount * byn_rub_course;
     return resultOfExchange;
 }
 double byn_uah(double &exchangeAmount, double &resultOfExchange)
 {
     cout << "What amount in BYN do you want to exchange?"<<endl;
     cin >> exchangeAmount;
-    resultOfExchange = exchangeAmount * 1.9;
+    resultOfExchange = exchangeAmount * byn_uah_course;
     return resultOfExchange;
 }
 double rub_eur(double &exchangeAmount, double &resultOfExchange)
 {
     cout << "What amount in RUB do you want to exchange?"<<endl;
     cin >> exchangeAmount;
-    resultOfExchange = exchangeAmount * 1.9;
+    resultOfExchange = exchangeAmount * rub_eur_course;
     return resultOfExchange;
 }
 double rub_usd(double &exchangeAmount, double &resultOfExchange)
 {
     cout << "What amount in RUB do you want to exchange?"<<endl;
     cin >> exchangeAmount;
-    resultOfExchange = exchangeAmount * 1.9;
+    resultOfExchange = exchangeAmount * rub_usd_course;
     return resultOfExchange;
 }
 double rub_byn(double &exchangeAmount, double &resultOfExchange)
 {
     cout << "What amount in RUB do you want to exchange?"<<endl;
     cin >> exchangeAmount;
-    resultOfExchange = exchangeAmount * 1.9;
+    resultOfExchange = exchangeAmount * rub_byn_course;
     return resultOfExchange;
 }
 double rub_uah(double &exchangeAmount, double &resultOfExchange)
 {
     cout << "What amount in RUB do you want to exchange?"<<endl;
     cin >> exchangeAmount;
-    resultOfExchange = exchangeAmount * 1.9;
+    resultOfExchange = exchangeAmount * rub_uah_course;
     return resultOfExchange;
 }
 double uah_eur(double &exchangeAmount, double &resultOfExchange)
 {
     cout << "What amount in UAH do you want to exchange?"<<endl;
     cin >> exchangeAmount;
-    resultOfExchange = exchangeAmount * 1.9;
+    resultOfExchange = exchangeAmount * uah_eur_course;
     return resultOfExchange;
 }
 double uah_usd(double &exchangeAmount, double &resultOfExchange)
 {
     cout << "What amount in UAH do you want to exchange?"<<endl;
     cin >> exchangeAmount;
-    resultOfExchange = exchangeAmount * 1.9;
+    resultOfExchange = exchangeAmount * uah_usd_course;
     return resultOfExchange;
 }
 double uah_byn(double &exchangeAmount, double &resultOfExchange)
 {
     cout << "What amount in UAH do you want to exchange?"<<endl;
     cin >> exchangeAmount;
-    resultOfExchange = exchangeAmount * 1.9;
+    resultOfExchange = exchangeAmount * uah_byn_course;
     return resultOfExchange;
 }
 double uah_rub(double &exchangeAmount, double &resultOfExchange)
 {
     cout << "What amount in UAH do you want to exchange?"<<endl;
     cin >> exchangeAmount;
-    resultOfExchange = exchangeAmount * 1.9;
+    resultOfExchange = exchangeAmount * uah_rub_course;
     return resultOfExchange;
 }
 
